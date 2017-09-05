af7eeccd5102ccad0f2b5920919372c7e1b432c0
nixCodeX <jonathan@mljp.me.uk>
2017-09-05 17:00:08 +0100

    Initial Commit

---
+++ Window/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Window
+//
+//  Created by Jonathan Tanner on 05/09/2017.
+//  Copyright © 2017 Jonathan Tanner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
